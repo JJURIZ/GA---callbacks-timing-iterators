07aeed2fe411e6326696a5ae9a4b4ca8105765ae
Jennifer Uriz <jenniferuriz@Jennifers-Air.attlocal.net>
2020-10-22 10:07:28 -0400

    [checkNumber] final 5 functions created.

--- checkNumber.js
+++ checkNumber.js
@@ -58,3 +58,62 @@ function checkNumber(num) {
 
 console.log(checkNumber(0));
 
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+
+console.log(checkNumber(345.6));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+
+console.log(checkNumber(-34.7654));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+
+console.log(checkNumber(90));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+
+console.log(checkNumber(-9));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+
+console.log(checkNumber(10000000000));
