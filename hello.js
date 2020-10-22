0f2521f72c8dedf2d474f20b7d7bdf959e9e299b
Jennifer Uriz <jenniferuriz@Jennifers-Air.attlocal.net>
2020-10-22 09:50:47 -0400

    [hello.js] First five functions complete.

---
+++ hello.js
@@ -0,0 +1,34 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Michael'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Tilly'));
+
+function sayHello(name) {
+    let msg = "Hello, " + name + '. How are you?';
+    return msg;
+};
+
+console.log(sayHello('Detmer'));
+
+function sayHello(name) {
+    let msg = "Hello, " + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello("Saru"));
